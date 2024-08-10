04bf558a750ebdc53f4370dbb6d96fcb37a1cbc2
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-08-10 16:41:40 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object("config.settings")
-
-db = SQLAlchemy(app)
-
-from app import routes, models, services
--- app/utils/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    API_KEY = "YOUR_API_KEY"
-    GITHUB_TOKEN = "YOUR_GITHUB_TOKEN"
-    DB_URI = "sqlite:///database.db"
