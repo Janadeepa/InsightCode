c141314c2b270af66af5ed681196d50e9044e8b0
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-08-10 16:50:10 +0530

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+# models/__init__.py
+
+# Initialize the models module.
+# You can import models here to make them available when the module is imported.
+from .code_analysis import analyze_code
+from .nlp_feedback import generate_feedback
