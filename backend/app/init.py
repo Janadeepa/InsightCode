8e2fda43e404cedb234a6999ffac692ddf9de7dd
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-08-10 16:35:34 +0530

    Update init.py

--- backend/app/init.py
+++ backend/app/init.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config.from_object("config.settings")
+
 db = SQLAlchemy(app)
 
-from . import routes, models, services, utils
+from app import routes, models, services
